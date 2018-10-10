80ee9298972c70ed4af30a06ff6ac3d88586460e
Khine <34533081+Khinexin@users.noreply.github.com>
2018-10-10 20:51:37 +0630

    day10 October.2018 message

---
+++ src/main/java/com/demo/mobileproject/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.demo.mobileproject.repository;
+
+
+import com.demo.demostudentmanagement.domain.Level;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+}
---
+++ src/main/java/com/demo/mobileproject/service/AdminService.java
@@ -0,0 +1,9 @@
+package com.demo.mobileproject.service;
+
+import com.demo.demostudentmanagement.domain.Admin;
+
+import java.util.List;
+
+public interface AdminService {
+   
+}
