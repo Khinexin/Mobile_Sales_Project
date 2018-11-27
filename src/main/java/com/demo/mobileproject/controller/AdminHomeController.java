cf666b44dce4ef65f6e48a122ee7e640262e058a
Khine <34533081+Khinexin@users.noreply.github.com>
2018-11-27 11:10:57 +0630

    secure login -- finish4

--- src/main/java/com/demo/mobileproject/controller/AdminHomeController.java
+++ src/main/java/com/demo/mobileproject/controller/AdminHomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WelcomeController {
+public class AdminHomeController {
 
     @GetMapping("/admin/layout")
     public String checkAdminLayout(){
