f2668c92e1e49ae3e834dd76debb134daa3b9b79
Khine <34533081+Khinexin@users.noreply.github.com>
2018-11-30 23:04:15 +0630

    changed the easy way userRepository and roleRepository

--- src/main/java/com/demo/mobileproject/domain/login/AppRoleDAO.java
+++ src/main/java/com/demo/mobileproject/domain/login/AppRoleDAO.java
@@ -2,28 +2,36 @@ package com.demo.mobileproject.domain.login;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+//@Repository
+//@Transactional
+//@PropertySource({"classpath:application.properties"})
+//public class AppRoleDAO {
+//
+//    @Autowired
+//    private EntityManager entityManager;
+//
+//    public List<String> getRoleNames(Integer userId) {
+//        String sql = "Select ur.appRole.roleName from " + UserRole.class.getName() + " ur " //
+//                + " where ur.appUser.userId = :userId ";
+//
+//        Query query = this.entityManager.createQuery(sql, String.class);
+//        query.setParameter("userId", userId);
+//        return query.getResultList();
+//    }
+//
+//}
+
 @Repository
-@Transactional
-@PropertySource({"classpath:application.properties"})
-public class AppRoleDAO {
-
-    @Autowired
-    private EntityManager entityManager;
-
-    public List<String> getRoleNames(Integer userId) {
-        String sql = "Select ur.appRole.roleName from " + UserRole.class.getName() + " ur " //
-                + " where ur.appUser.userId = :userId ";
-
-        Query query = this.entityManager.createQuery(sql, String.class);
-        query.setParameter("userId", userId);
-        return query.getResultList();
-    }
-
+public interface AppRoleDAO extends JpaRepository<AppRole, Integer> {
+    @Query("SELECT ur.appRole.roleName FROM UserRole ur WHERE ur.appUser.userId = :userId ")
+    List<String> getRoleNames(@Param("userId") Integer userId);
 }
--- src/main/java/com/demo/mobileproject/domain/login/AppUserDAO.java
+++ src/main/java/com/demo/mobileproject/domain/login/AppUserDAO.java
@@ -2,32 +2,42 @@ package com.demo.mobileproject.domain.login;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import javax.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+//@Repository
+//@Transactional
+//@PropertySource({"classpath:application.properties"})
+//public class AppUserDAO {
+//
+//    @Autowired
+//    private EntityManager entityManager;
+//
+//    public AppUser findUserAccount(String userName) {
+//        try {
+//            String sql = "Select e from " + AppUser.class.getName() + " e " //
+//                    + " Where e.userName = :userName ";
+//
+//            Query query = entityManager.createQuery(sql, AppUser.class);
+//            query.setParameter("userName", userName);
+//
+//            return (AppUser) query.getSingleResult();
+//        } catch (NoResultException e) {
+//            return null;
+//        }
+//    }
+//
+//}
+
 @Repository
-@Transactional
-@PropertySource({"classpath:application.properties"})
-public class AppUserDAO {
-
-    @Autowired
-    private EntityManager entityManager;
-
-    public AppUser findUserAccount(String userName) {
-        try {
-            String sql = "Select e from " + AppUser.class.getName() + " e " //
-                    + " Where e.userName = :userName ";
-
-            Query query = entityManager.createQuery(sql, AppUser.class);
-            query.setParameter("userName", userName);
-
-            return (AppUser) query.getSingleResult();
-        } catch (NoResultException e) {
-            return null;
-        }
-    }
+public interface AppUserDAO extends JpaRepository<AppUser,Integer> {
+    @Query("SELECT e FROM AppUser e WHERE e.userName = :userName")
+    AppUser findUserAccount(@Param("userName") String userName);
 
 }
